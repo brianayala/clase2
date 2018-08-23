78a3ea9c11c4d36fcf76e499d6635159ed7b81fc
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-08-23 20:59:53 -0300

    un comentario

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int numeroUno;
+int numeroDos;
+int numeroTres;
+int i;
+int numeroMax;
+int numeroMin;
+for(i=0;i<10;i++;) {
+printf("ingrese el numero 1");
+scanf("%d",&numeroUno);
+if (i==0){
+numeroMax=numeroUno;
+numeroMin=numeroUno;
+}
+
+else {
+    if (numeroUno>numeroMax){
+    numeroMax=numeroUno;
+    }else if (numeroUno<numeroMin){
+    numeroMin=numeroUno;
+    }
+  printf("el")
+
+}
+
+
+
+
+}
+
+
